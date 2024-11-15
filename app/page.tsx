ba5ad6ec916be4d3fdb971124e2e81765417e41f
johntarus <tarusjohn96@gmail.com>
2024-11-15 23:34:36 +0300

    index page

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly / in real time.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
